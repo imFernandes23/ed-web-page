218e5276271ec38cf5d5f627bf0bbdc7afefde89
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2022-11-30 20:59:15 -0300

    .

--- src/components/CircularCard.js
+++ src/components/CircularCard.js
@@ -14,30 +14,49 @@ export default function CircularCard(props){
     //Elements props
     const icon = props.icon
     const color = props.color
+    const percent = props.percent
+    const wave100 = 'M 0 400 L 0 400 L 0 50 C 50 0 50 100 100 50 C 150 0 150 100 200 50 L 200 400 L 0 400'
+    const wave0 = 'M 0 400 L 0 400 L 0 350 C 50 300 50 400 100 350 C 150 300 150 400 200 350 L 200 400 L 0 400 '
+    const waveStyle1 = `path('${wave0}')`
+
 
 
     return(
         
 
         <div ref={Ref} className='circular-card active'>
-            <div className={!isOpen ? 'button ' : 'button active'} onClick={() => setIsOpen(!isOpen)} style={{'--MainColor':color}}>
+            <div className={!isOpen ? 'button ' : 'button active'} onClick={() => setIsOpen(!isOpen)} >
                 <svg >
                     <circle cx={65} cy={65} r={65}></circle>
                 </svg>
 
-                {props.icon}   
+                {icon}   
 
             </div> 
 
             <div className={ isOpen ? 'full-card active' : 'full-card'} onClick={() => setIsOpen(!isOpen)}>
 
-                <div className="bar-vert-icon">
+                <div className="bar-vert-icon"  style={{'--MainColor':color , '--Percent': percent, '--Wave1' : waveStyle1 }}>
+                    {icon}
+
+                    <div className="wave1" >
+                        {icon}
+                    </div>
 
                 </div>
                 <div className="card-itens-list">
                 </div>
-                <div className="percent">
-                    
+
+
+                <div className="percent" style={{'--MainColor':color , '--Percent': percent}}>
+                <div className="dot"></div>
+                <svg>
+                    <circle cx={50} cy={50} r={50}  ></circle>
+                    <circle cx={50} cy={50} r={50}  ></circle>
+                </svg>
+                <p>{percent}%</p>
+
+                
                 </div>
 
             </div>
--- src/pages/Knowledge.js
+++ src/pages/Knowledge.js
@@ -9,12 +9,12 @@ function Knowledge(props) {
         <div className={props.state > 2 ? 'knowledge page' : 'knowledge page active'} onClick={() =>  props.click(2)}>
             <div className={props.state === 2 ? 'content active' : 'content'}>
 
-                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'}/>
-                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'}/>
-                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'}/>
-                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'}/>
-                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'}/>
-                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'}/>
+                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'} percent={85}/>
+                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'} percent={85}/>
+                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'} percent={85}/>
+                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'} percent={85}/>
+                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'} percent={85}/>
+                <CircularCard icon={<DiIcons.DiReact/> } color={'#00EEF2'} percent={85}/>
 
             </div>
         </div>
