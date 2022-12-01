3548b1e6721c4050ef41bc2d216f4880957f017e
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2022-12-01 14:36:30 -0300

    .

--- src/components/ColorDrop.js
+++ src/components/ColorDrop.js
@@ -1,11 +1,16 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import './css/ColorDrop.css'
+import OutSideClick from "../hooks/OutsideClick";
 
 function ColorDrop(props){
     const [color, setColor] = useState(false)
+    const Ref = useRef(null)
+    const close = () => setColor(false)
+
+    OutSideClick(Ref, color, close)
 
 return(
-    <div className={ color ? 'color-drop active' : 'color-drop' }    
+    <div ref={Ref} className={ color ? 'color-drop active' : 'color-drop' }    
         onClick={() => setColor(!color)}
         style={{'--MainColor':props.color , '--PosiX':props.posiX, '--PosiX2': props.posiX2 }}>
 
